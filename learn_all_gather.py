c86bd269766bf0a01e7615038c7fc50435d402cd
saurabh <thethreadpool@gmail.com>
2018-12-25 16:38:19 -0600

    All reduce code working

--- learn_all_gather.py
+++ learn_all_gather.py
@@ -1,10 +1,10 @@
 import torch
 import torch.distributed as dist
 
-dist.init_process_group(backend="nccl", init_method="file:///distributed_test",
+dist.init_process_group(backend="nccl", init_method="file:///home/saurabh/pytorch_distributed_experiments/distributed_test",
                         world_size = 2, rank=0)
 
-temp_tensor = torch.FloatTensor[0.5].cuda(0)
+temp_tensor = torch.FloatTensor([0.5]).cuda(0)
 tensor_list = list()
 tensor_list.append(temp_tensor)
 dist.all_reduce_multigpu(tensor_list)
--- learn_all_gather_1.py
+++ learn_all_gather_1.py
@@ -1,10 +1,10 @@
 import torch
 import torch.distributed as dist
 
-dist.init_process_group(backend="nccl", init_method="file:///distributed_test",
+dist.init_process_group(backend="nccl", init_method="file:///home/saurabh/pytorch_distributed_experiments/distributed_test",
                         world_size = 2, rank=1)
 
-temp_tensor = torch.FloatTensor[1].cuda(1)
+temp_tensor = torch.FloatTensor([1]).cuda(1)
 tensor_list = list()
 tensor_list.append(temp_tensor)
 dist.all_reduce_multigpu(tensor_list)
