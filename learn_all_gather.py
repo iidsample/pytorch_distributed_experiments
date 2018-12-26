cbc92e34599ac47393f833024e80797ac2de2951
Saurabh <saurabh@madstreetden.com>
2018-12-25 20:46:45 -0600

    Modified torch.distributed.launch

--- learn_all_gather.py
+++ learn_all_gather.py
@@ -1,16 +1,23 @@
 import torch
 import torch.distributed as dist
 import os
+import argparse
 
-os.environ["MASTER_ADDR"] = "127.0.0.1"
-os.environ["MASTER_PORT"] = "6066"
+parser = argparse.ArgumentParser()
+
+args = parser.parse_args()
+
+# os.environ["MASTER_ADDR"] = "127.0.0.1"
+# os.environ["MASTER_PORT"] = "6066"
+print ("Hello from local rank {}".format(args.local_rank))
 dist.init_process_group(backend="nccl", init_method="env://",
-                        world_size = 2, rank=0)
-out_tensor_list = [[torch.FloatTensor([0]).cuda(0),
-                    torch.FloatTensor([0]).cuda(0)]]
-temp_tensor = torch.FloatTensor([0.5]).cuda(0)
+                        world_size = 2, rank=args.local_rank)
+out_tensor_list = [[torch.FloatTensor([0]).cuda(args.local_rank),
+                    torch.FloatTensor([0]).cuda(args.local_rank)]]
+temp_tensor = torch.rand(1).cuda(args.local_rank)
+print("Tensor{} in device {}".format(temp_tensor, args.local_rank))
 tensor_list = list()
 tensor_list.append(temp_tensor)
 dist.all_gather_multigpu(out_tensor_list, tensor_list)
-print(tensor_list)
-print(out_tensor_list)
+# print(tensor_list)
+print("Out tensor {} on {}".format(out_tensor_list, args.local_rank)
