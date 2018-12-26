d70e361ada950dadb01359fa72ce5f044fd62762
Saurabh <saurabh@madstreetden.com>
2018-12-25 18:34:22 -0600

    Trying all gather

--- learn_all_gather.py
+++ learn_all_gather.py
@@ -3,10 +3,11 @@ import torch.distributed as dist
 
 dist.init_process_group(backend="nccl", init_method="file:///home/saurabh/pytorch_distributed_experiments/distributed_test",
                         world_size = 2, rank=0)
-
+out_tensor_list = [[torch.FloatTensor([0]).cuda(0),
+                    torch.FloatTensor([0]).cuda(0)]]
 temp_tensor = torch.FloatTensor([0.5]).cuda(0)
 tensor_list = list()
 tensor_list.append(temp_tensor)
-dist.all_reduce_multigpu(tensor_list)
+dist.all_gather_multigpu(out_tensor_list, tensor_list)
 print(tensor_list)
 print(temp_tensor)
--- learn_all_gather_1.py
+++ learn_all_gather_1.py
@@ -3,10 +3,12 @@ import torch.distributed as dist
 
 dist.init_process_group(backend="nccl", init_method="file:///home/saurabh/pytorch_distributed_experiments/distributed_test",
                         world_size = 2, rank=1)
+out_tensor_list = [[torch.FloatTensor([0]).cuda(0),
+                    torch.FloatTensor([0]).cuda(0)]]
 
 temp_tensor = torch.FloatTensor([1]).cuda(1)
 tensor_list = list()
 tensor_list.append(temp_tensor)
-dist.all_reduce_multigpu(tensor_list)
+dist.all_gather_multigpu(out_tensor_list, tensor_list)
 print(tensor_list)
 print(temp_tensor)
