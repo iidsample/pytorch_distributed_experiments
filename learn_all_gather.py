4f66e5473b7b422e90f34f02b6e225e2cf441b2f
Saurabh <saurabh@madstreetden.com>
2018-12-25 19:26:40 -0600

    Added environ for os

--- learn_all_gather.py
+++ learn_all_gather.py
@@ -1,6 +1,8 @@
 import torch
 import torch.distributed as dist
 
+os.environ["MASTER_ADDR"] = "192.168.0.5"
+os.environ["MASTER_PORT"] = "6066"
 dist.init_process_group(backend="nccl", init_method="file:///home/saurabh/pytorch_distributed_experiments/distributed_test",
                         world_size = 2, rank=0)
 out_tensor_list = [[torch.FloatTensor([0]).cuda(0),
--- learn_all_gather_1.py
+++ learn_all_gather_1.py
@@ -1,6 +1,9 @@
 import torch
 import torch.distributed as dist
+import os
 
+os.environ["MASTER_ADDR"] = "192.168.0.5"
+os.environ["MASTER_PORT"] = "6066"
 dist.init_process_group(backend="nccl", init_method="file:///home/saurabh/pytorch_distributed_experiments/distributed_test",
                         world_size = 2, rank=1)
 out_tensor_list = [[torch.FloatTensor([0]).cuda(1),
