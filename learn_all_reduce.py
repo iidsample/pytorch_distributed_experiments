0c7ea2d1684b38a1d14ea085c137957d24736198
Ubuntu <ubuntu@ip-172-31-28-191.ec2.internal>
2019-04-13 23:16:54 +0000

    All reduce testing code

---
+++ learn_all_reduce.py
@@ -0,0 +1,27 @@
+import torch
+import torch.distributed as dist
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--local_rank', default=0, type=int)
+args = parser.parse_args()
+
+# os.environ["MASTER_ADDR"] = "127.0.0.1"
+# os.environ["MASTER_PORT"] = "6066"
+print ("World size  {}".format(os.environ["WORLD_SIZE"]))
+print ("Hello from local rank {}".format(args.local_rank))
+dist.init_process_group(backend="nccl", init_method="env://",
+                        world_size = int(os.environ["WORLD_SIZE"]), rank=args.local_rank)
+# out_tensor_list = [[torch.FloatTensor([0]).cuda(args.local_rank),
+                    # torch.FloatTensor([0]).cuda(args.local_rank)]]
+out_tensor_list = [[
+    torch.FloatTensor([0]).cuda(args.local_rank) for x in
+    range(int(os.environ["WORLD_SIZE"]))]]
+temp_tensor = torch.rand(1).cuda(args.local_rank)
+print("Tensor{} in device {}".format(temp_tensor, args.local_rank))
+tensor_list = list()
+tensor_list.append(temp_tensor)
+dist.all_reduce_multigpu(tensor_list)
+print("tensor list = {} on {}".format(tensor_list, args.local_rank))
+print("Out tensor {} on {}".format(out_tensor_list, args.local_rank))
