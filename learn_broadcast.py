783c507721b42a5e0cec7b9d1a52eaad33ee23d6
Saurabh <thethreadpool@gmail.com>
2020-01-15 23:01:26 -0600

    Added test for broadcast

---
+++ learn_broadcast.py
@@ -0,0 +1,22 @@
+import torch
+import torch.distributed as dist
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--local-rank', default=0, type=int)
+parser.add_argument("--master-ip", type=str)
+parser.add_argument("--world-size", type=int)
+args = parser.parse_args()
+dist.init_process_group(backend="nccl", init_method=args.local_rank,
+                        world_size=args.world_size, rank=args.local_rank)
+
+test_tensor = torch.zeros((4,4), device='cuda:0')
+
+print (test_tensor)
+if args.local_rank == 0:
+    test_tensor[0,0] = 4
+    test_tensor[1,1] = 2
+
+torch.distributed.broadcast(test_tensor, 0)
+print (test_tensor)
